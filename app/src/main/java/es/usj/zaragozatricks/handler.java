2e1aae06a90394b633f8cf2f43d5743cfbbf4e09
Roger Vallejo <rogerbalbuena@hotmail.com>
2018-12-16 21:33:15 +0100

    removing hardcoded text in handler

--- app/src/main/java/es/usj/zaragozatricks/handler.java
+++ app/src/main/java/es/usj/zaragozatricks/handler.java
@@ -1,14 +1,12 @@
 package es.usj.zaragozatricks;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.net.Uri;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 
@@ -34,44 +32,48 @@ public class handler extends AppCompatActivity {
         int fragmentID = i.getIntExtra("fragmentID",0);
         title = findViewById(R.id.txtTitle2);
         manageFragment(fragmentID);
-
-
-
     }
 
     public void manageFragment(int fragment){
-        if (fragment == R.layout.fragment_lugares){
-            title.setText("Lugares");
-            LugaresFragment lugaresFragment = new LugaresFragment();
-            FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(fragment_container, lugaresFragment);
-            fragmentTransaction.commit();
-        }else if (fragment == R.layout.fragment_universidad){
-            title.setText("Universidad");
-            UniversidadFragment universidadFragment = new UniversidadFragment();
-            FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(fragment_container, universidadFragment);
-            fragmentTransaction.commit();
-        }else if (fragment == R.layout.fragment_transporte) {
-            title.setText("Transporte");
-            TransporteFragment transporteFragment = new TransporteFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(fragment_container, transporteFragment);
-            fragmentTransaction.commit();
-        }else if (fragment == R.layout.fragment_nie) {
-            title.setText("N.I.E");
-            NIEFragment nieFragment = new NIEFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(fragment_container, nieFragment);
-            fragmentTransaction.commit();
-        }else if (fragment == R.layout.fragment_empadronarse) {
-            title.setText("Empadronarse");
-            EmpadronarseFragment empadronarseFragment = new EmpadronarseFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(fragment_container, empadronarseFragment);
-            fragmentTransaction.commit();
+
+        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        switch (fragment){
+            case R.layout.fragment_lugares:
+                title.setText(getString(R.string.lugares));
+                LugaresFragment lugaresFragment = new LugaresFragment();
+                fragmentTransaction.add(fragment_container, lugaresFragment);
+                break;
+            case R.layout.fragment_universidad:
+                title.setText(getString(R.string.universidad));
+                UniversidadFragment universidadFragment = new UniversidadFragment();
+                fragmentTransaction.add(fragment_container, universidadFragment);
+                break;
+            case R.layout.fragment_transporte:
+                title.setText(getString(R.string.transporte));
+                TransporteFragment transporteFragment = new TransporteFragment();
+                fragmentTransaction.add(fragment_container, transporteFragment);
+                break;
+            case R.layout.fragment_nie:
+                title.setText(getString(R.string.n_i_e));
+                NIEFragment nieFragment = new NIEFragment();
+                fragmentTransaction.add(fragment_container, nieFragment);
+                break;
+            case R.layout.fragment_empadronarse:
+                title.setText(getString(R.string.empadronarse));
+                EmpadronarseFragment empadronarseFragment = new EmpadronarseFragment();
+                fragmentTransaction.add(fragment_container, empadronarseFragment);
+                break;
         }
 
+
+
+        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+        fragmentTransaction.commit();
     }
 
 }
