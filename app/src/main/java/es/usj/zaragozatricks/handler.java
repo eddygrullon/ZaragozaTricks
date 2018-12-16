db4a3dabd2706594d3840626c7011b41be46469d
Roger Vallejo <rogerbalbuena@hotmail.com>
2018-12-16 18:04:24 +0100

    back button

--- app/src/main/java/es/usj/zaragozatricks/handler.java
+++ app/src/main/java/es/usj/zaragozatricks/handler.java
@@ -2,13 +2,24 @@ package es.usj.zaragozatricks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class handler extends AppCompatActivity {
 
+    Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler);
+
+        btnback = findViewById(R.id.btn_back);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
