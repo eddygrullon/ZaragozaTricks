f92c1dc4de66ba807fede35105fb52a9be795a54
Eddy Grullón <eddygrullon@outlook.es>
2018-12-16 19:22:08 +0100

    Permisos de la camara

--- app/src/main/java/es/usj/zaragozatricks/MainActivity.java
+++ app/src/main/java/es/usj/zaragozatricks/MainActivity.java
@@ -7,13 +7,10 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 
-
-
 public class MainActivity extends AppCompatActivity  {
 
     private CardView cv_empadronarse, cv_nie,cv_transporte, cv_deInteres,cv_universidad,cv_lugares;
     private int fragment_id;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +23,6 @@ public class MainActivity extends AppCompatActivity  {
         cv_deInteres = findViewById(R.id.cv_deInteres);
         cv_universidad =  findViewById(R.id.cv_universidad);
         cv_lugares = findViewById(R.id.cv_lugares);
-
-
 
         cv_nie.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,30 +57,22 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 callHandler(R.layout.fragment_universidad);
-
             }
         });
         cv_lugares.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callHandler(R.layout.fragment_lugares);
-
             }
         });
-
-
     }
 
-
     private void callHandler(int fragment){
-
-
 
         Intent intent = new Intent(this,handler.class);
         intent.putExtra("fragmentID", fragment);
 
-        startActivity(intent,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 
 }
